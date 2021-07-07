e0fee7a3e8997afdfc6a36411c958e48b8747c8c
Aymane Benhima <aymanebenhima@gmail.com>
2021-07-07 15:49:26 +0100

    display info only for auth user middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,8 +1,20 @@
-const expressJWT = require('express-jwt');
-require('dotenv').config();
+const expressJWT = require("express-jwt");
+require("dotenv").config();
 
 exports.requireSignIn = expressJWT({
-    secret: process.env.JWT_SECRET,
-    algorithms: ["HS256"],
-    userProperty: 'auth',
+  secret: process.env.JWT_SECRET,
+  algorithms: ["HS256"],
+  userProperty: "auth",
 });
+
+exports.isAuth = (req, res, next) => {
+  let user = req.profile && req.auth && req.profile._id == req.auth._id;
+
+  if (!user) {
+    return res.status(403).json({
+      error: "Access denied",
+    });
+  }
+
+  next();
+};
