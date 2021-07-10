4eb5550635f2641e7d570eb30cc3959950ce1b9d
Aymane Benhima <aymanebenhima@gmail.com>
2021-07-10 22:11:33 +0100

    crud category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -15,3 +15,66 @@ exports.createCategory = (req, res) => {
     });
   });
 };
+
+exports.categoryById = (req, res, next, id) => {
+  Category.findById(id).exec((err, category) => {
+    if (err || !category) {
+      return res.status(404).json({
+        error: "Category not found !",
+      });
+    }
+
+    req.category = category;
+    next();
+  });
+};
+
+exports.showCategory = (req, res) => {
+  res.json({
+    category: req.category,
+  });
+};
+
+exports.removeCategory = (req, res) => {
+  let category = req.category;
+
+  category.remove((err, category) => {
+    if (err) {
+      return res.status(404).json({
+        error: "category not deleted !",
+      });
+    }
+    res.status(204).json({});
+  });
+};
+
+exports.updateCategory = (req, res) => {
+  const category = req.category;
+
+  category.name = req.body.name;
+
+  category.save((err, category) => {
+    if (err) {
+      return res.status(400).json({
+        error: "category not updated !",
+      });
+    }
+
+    res.json({
+      category: category,
+    });
+  });
+};
+
+exports.allCategories = (req, res) => {
+  Category.find().exec((err, categories) => {
+    if (err) {
+      return res.status(500).json({
+        error: err,
+      });
+    }
+    res.json({
+      categories,
+    });
+  });
+};
--- routes/categories.js
+++ routes/categories.js
@@ -1,11 +1,35 @@
 const express = require("express");
-const { createCategory } = require('../controllers/categoryController');
+const {
+  allCategories,
+  createCategory,
+  showCategory,
+  categoryById,
+  updateCategory,
+  removeCategory,
+} = require("../controllers/categoryController");
 const { userById } = require("../middlewares/user");
 const { requireSignIn, isAuth, isAdmin } = require("../middlewares/auth");
 
 const router = express.Router();
 
-router.post('/create/:userId', [requireSignIn, isAuth, isAdmin], createCategory);
+router.get("/", allCategories);
+router.get("/:categoryId", showCategory);
+router.post(
+  "/create/:userId",
+  [requireSignIn, isAuth, isAdmin],
+  createCategory
+);
+router.put(
+  "/:categoryId/:userId",
+  [requireSignIn, isAuth, isAdmin],
+  updateCategory
+);
+router.delete(
+  "/:categoryId/:userId",
+  [requireSignIn, isAuth, isAdmin],
+  removeCategory
+);
 router.param("userId", userById);
+router.param("categoryId", categoryById);
 
 module.exports = router;
